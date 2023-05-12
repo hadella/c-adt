ca39b2473c124681091cb6df106a6f072621ddf1
hadella <justin.hadella@gmail.com>
2023-05-11 19:41:51 -0600

    update list test

--- tests/list_test.c
+++ tests/list_test.c
@@ -70,7 +70,7 @@ Test(list_tests, insert_after_head)
 	cr_expect(list_data(list_tail(&list)) == item2, "tail should be the second item inserted");
 }
 
-Test(list_tests, insert_twice_at_head)
+Test(list_tests, insert_before_head)
 {
 	// Give out items some values
 	*item1 = 1;
@@ -78,7 +78,7 @@ Test(list_tests, insert_twice_at_head)
 
 	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
 	cr_expect(list_size(&list) == 1, "list's size should be 1");
-	cr_expect(list_insert_next(&list, NULL, item2) == 0, "insert after head should return 0");
+	cr_expect(list_insert_next(&list, NULL, item2) == 0, "insert before head should return 0");
 	cr_expect(list_size(&list) == 2, "list's size should be 2");
 	cr_expect(list_head(&list) != NULL, "list's head should be not NULL");
 	cr_expect(list_tail(&list) != NULL, "list's tail should be not NULL");
@@ -86,24 +86,6 @@ Test(list_tests, insert_twice_at_head)
 	cr_expect(list_next(list_tail(&list)) == NULL, "next should be NULL for tail of two element list");
 	cr_expect(list_data(list_head(&list)) == item2, "head should be the second item inserted");
 	cr_expect(list_data(list_tail(&list)) == item1, "tail should be the first item inserted");
-}
-
-Test(list_tests, insert_twice_at_tail)
-{
-	// Give out items some values
-	*item1 = 1;
-	*item2 = 2;
-
-	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
-	cr_expect(list_size(&list) == 1, "list's size should be 1");
-	cr_expect(list_insert_next(&list, list_tail(&list), item2) == 0, "insert after tail should return 0");
-	cr_expect(list_size(&list) == 2, "list's size should be 2");
-	cr_expect(list_head(&list) != NULL, "list's head should be not NULL");
-	cr_expect(list_tail(&list) != NULL, "list's tail should be not NULL");
-	cr_expect(list_next(list_head(&list)) != NULL, "next should be not NULL for head of two element list");
-	cr_expect(list_next(list_tail(&list)) == NULL, "next should be NULL for tail of two element list");
-	cr_expect(list_data(list_head(&list)) == item1, "head should be the first item inserted");
-	cr_expect(list_data(list_tail(&list)) == item2, "tail should be the second item inserted");
 }
 
 Test(list_tests, remove_at_head)
@@ -117,12 +99,17 @@ Test(list_tests, remove_at_head)
 
 	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
 	cr_expect(list_size(&list) == 1, "list's size should be 1");
-	cr_expect(list_insert_next(&list, NULL, item2) == 0, "insert after head should return 0");
+	cr_expect(list_insert_next(&list, NULL, item2) == 0, "insert before head should return 0");
 	cr_expect(list_size(&list) == 2, "list's size should be 2");
 	cr_expect(list_remove_next(&list, NULL, &removed) == 0, "remove at head should return 0");
 	cr_expect(list_size(&list) == 1, "list's size should be 1");
 	cr_expect(list_data(list_head(&list)) == item1, "head should be the first item inserted");
 	cr_expect(removed == item2, "removed item should point to the second item inserted");
+	cr_expect(list_remove_next(&list, NULL, &removed) == 0, "remove at head should return 0");
+	cr_expect(list_size(&list) == 0, "list's size should be 0");
+	cr_expect(removed == item1, "removed item should point to the first item inserted");
+	cr_expect(list_head(&list) == NULL, "empty list's head should be NULL");
+	cr_expect(list_tail(&list) == NULL, "empty list's tail should be NULL");
 }
 
 Test(list_tests, remove_after_head)
@@ -142,13 +129,22 @@ Test(list_tests, remove_after_head)
 	cr_expect(list_size(&list) == 1, "list's size should be 1");
 	cr_expect(list_data(list_head(&list)) == item2, "head should be the second item inserted");
 	cr_expect(removed == item1, "removed item should point to the first item inserted");
+	// Attempt to remove again at head (this should fail)
+	cr_expect(list_remove_next(&list, list_head(&list), &removed) == -1, "remove at head should return -1");
+	// Do it right...
+	cr_expect(list_remove_next(&list, NULL, &removed) == 0, "remove at head should return 0");
+	cr_expect(list_size(&list) == 0, "list's size should be 0");
+	cr_expect(removed == item2, "removed item should point to the second item inserted");
+	cr_expect(list_head(&list) == NULL, "empty list's head should be NULL");
+	cr_expect(list_tail(&list) == NULL, "empty list's tail should be NULL");
 }
 
-Test(list_tests, add_and_remove)
+Test(list_tests, remove_in_between)
 {
 	// Give out items some values
 	*item1 = 1;
 	*item2 = 2;
+	*item3 = 3;
 
 	// Pointer to removed item
 	void* removed;
@@ -157,14 +153,11 @@ Test(list_tests, add_and_remove)
 	cr_expect(list_size(&list) == 1, "list's size should be 1");
 	cr_expect(list_insert_next(&list, NULL, item2) == 0, "insert after head should return 0");
 	cr_expect(list_size(&list) == 2, "list's size should be 2");
-
-	cr_expect(list_remove_next(&list, NULL, &removed) == 0, "remove at head should return 0");
-	cr_expect(list_size(&list) == 1, "list's size should be 1");
-	cr_expect(list_data(list_head(&list)) == item1, "head should be the first item inserted");
+	cr_expect(list_insert_next(&list, NULL, item3) == 0, "insert after head should return 0");
+	cr_expect(list_size(&list) == 3, "list's size should be 2");
+	cr_expect(list_remove_next(&list, list_head(&list), &removed) == 0, "remove at head should return 0");
+	cr_expect(list_size(&list) == 2, "list's size should be 2");
+	cr_expect(list_data(list_head(&list)) == item3, "head should be the third item inserted");
 	cr_expect(removed == item2, "removed item should point to the second item inserted");
-
-	cr_expect(list_remove_next(&list, NULL, &removed) == 0, "remove at head should return 0");
-	cr_expect(list_size(&list) == 0, "list's size should be 0");
-	cr_expect(list_head(&list) == NULL, "head should be NULL");
-	cr_expect(removed == item1, "removed item should point to the first item inserted");
+	cr_expect(list_data(list_tail(&list)) == item1, "tail should be the first item inserted");
 }
